41e86214dccdb8382034280e9ea6c8e4a7487030
KMMFestin10 <202380363@psu.palawan.edu.ph>
2025-09-07 21:32:04 +0800

    modify allowed host

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qa9)u0mmjqc#e%z49he^$b7*oc#bka3nyn4f2#4e$4w(o4t=1@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
